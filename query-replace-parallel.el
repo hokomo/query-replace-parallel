f1566d1e38f165dcb0b598ea37f01773216c5a98
hokomo <hokomo@airmail.cc>
2023-03-15 17:37:03 +0100

    Add TODO

--- query-replace-parallel.el
+++ query-replace-parallel.el
@@ -198,6 +198,22 @@
 ;;   meddling with the internals of `replace.el'. Luckily this is all purely
 ;;   aesthetical, but it would be nice to get a warning if anything ever changes
 ;;   unexpectedly.
+;;
+;; - Add tests.
+;;
+;;   (defvar *test*
+;;     `(("a12" . "b12")
+;;       ("b12" . "c12")
+;;       ("c12" . "d12")
+;;       ("d12" . "a12")))
+;;
+;;   (defvar *test-regexp*
+;;     `((,(rx "a" (group digit (group digit))) . "\\2 \\1 \\?")
+;;       (,(rx "b" (group digit (group-n 4 digit))) . "\\1")
+;;       (,(rx "c" (group digit (group-n 1 digit))) . "\\1")
+;;       (,(rx "d" (group-n 3 digit) (group-n 3 digit)) . "\\3")))
+;;
+;;   Another good example is "stopwatch" -> "timer", "watch" -> "wristwatch".
 
 ;;; Code:
 
